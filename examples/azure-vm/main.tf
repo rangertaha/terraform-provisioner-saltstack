a8a20098ca8754e8e7542d38eb5b8ada5c0720b9
Tal Hachi <rangertaha@gmail.com>
2019-07-26 22:12:27 -0400

    removed old code

--- examples/azure-vm/main.tf
+++ examples/azure-vm/main.tf
@@ -81,17 +81,6 @@ resource "azurerm_network_security_group" "exampleterraformnsg" {
   location = "eastus"
   resource_group_name = "${azurerm_resource_group.exampleterraformgroup.name}"
 
-  //  security_rule {
-  //    name = "SSH"
-  //    priority = 1001
-  //    direction = "Inbound"
-  //    access = "Allow"
-  //    protocol = "Tcp"
-  //    source_port_range = "*"
-  //    destination_port_range = "21-9200"
-  //    source_address_prefix = "158.81.192.22"
-  //    destination_address_prefix = "*"
-  //  }
   security_rule {
     name = "SSH"
     priority = 1001
